4c56895237478177ca101ff53e60265854188494
Samuel Vasko <samvasko@gmail.com>
2013-12-31 15:23:24 +0100

    Cleanup of the cleanup file

--- gitcleanup.rb
+++ gitcleanup.rb
@@ -1,26 +1,16 @@
-def main()
-	git_dir = '\\vendor\\msysgit\\libexec\\git-core\\'
-	working_dir = Dir.pwd
-	Dir.chdir(working_dir + git_dir)
-	Dir.entries(working_dir + git_dir).each do |file|
-		if file == 'git.exe' or file == 'mergetools' or file.end_with?('.bat') then
-			next
+require "pp"
+
+git_dir = '/vendor/msysgit/libexec/git-core/'
+working_dir = Dir.pwd
+Dir.chdir(working_dir + git_dir)
+Dir.entries('.').each do |file|
+	if file.end_with?('.exe') and not (file == 'git.exe' or file == 'mergetools' or file.end_with?('.bat'))
+		File.open(File.basename(file, '.*') + '.bat', "w") do |new_file|
+			new_file.write('@' + File.basename(file, '.*').gsub('-',' ') + ' $*')
 		end
-		if file.end_with?('.exe') then
-			File.open(File.basename(file, '.*') + '.bat', "w") do |new_file|
-				new_file.write('@ECHO OFF\n' + File.basename(file, '.*').gsub('-',' ') + ' $*')
-			end
-			File.delete(file)
-			next
-		elsif file.end_with?('.bat') then
-			File.open(file + '.bat', "w") do |new_file|
-				new_file.write('@ECHO OFF\n' + file.gsub('-', ' ') + ' $*')
-			end
-			File.delete(file)
-			next
-		end
+		puts 'Deleting: ' + file
+		File.delete(file)
+		next
 	end
 end
-
-main
 
