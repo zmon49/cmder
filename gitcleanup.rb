989dd235202fc3ca935bc6fb4765f0fdc3f53852
Samuel Vasko <samvasko@gmail.com>
2014-01-02 22:26:51 +0100

    Forgotten params

--- gitcleanup.rb
+++ gitcleanup.rb
@@ -16,7 +16,7 @@ Dir.glob('*.exe').each do |file|
     if file != 'git.exe' and md5(file) == clone_md5
         command = file.match(/git-(.*).exe/)[1]
         filename = file.split('.')[0] + '.bat'
-        File.write(filename, '@git ' + command) unless File.exists?(filename)
+        File.write(filename, '@git ' + command + ' $*') unless File.exists?(filename)
         File.delete(file)
     end
 end
