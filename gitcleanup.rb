3e2d04404119ec71d4898de92d753b3dec506d12
Samuel Vasko <samvasko@gmail.com>
2014-01-02 22:21:28 +0100

    Working cleanup script for the duplicate files

--- gitcleanup.rb
+++ gitcleanup.rb
@@ -14,6 +14,11 @@ puts 'Md5 of the clones is: ' + clone_md5
 
 Dir.glob('*.exe').each do |file|
 	if file != 'git.exe' and md5(file) == clone_md5
-
+        command = file.match(/git-(.*).exe/)[1]
+        filename = file.split('.')[0] + '.bat'
+        File.write(filename, '@git ' + command) unless File.exists?(filename)
+        File.delete(file)
 	end
 end
+
+Dir.chdir(working_dir)
